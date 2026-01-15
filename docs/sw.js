ce84eafadbccc9083ddea3686dfdf71f2d3609ad
ataalla24-ux <ataalla24@gmail.com>
2026-01-15 16:56:23 +0100

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'freefinder-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.url.includes('deals.json')) {
+    e.respondWith(
+      fetch(e.request)
+        .then((res) => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+  }
+});
